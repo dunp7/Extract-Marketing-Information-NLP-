3af6509265e6ccea2ebcdcb6bbec6f4ded9951db
Dunp <dunp1710@gmail.com>
2024-03-22 15:32:21 +0700

    running LDA

--- Method/prep_pipeline.py
+++ Method/prep_pipeline.py
@@ -4,6 +4,7 @@
 Some of the common text preprocessing / cleaning steps are:
 - Tokenization: v
 - Lower casing: v
+- Removal of Numbers: v
 - Removal of Punctuations:  v
 - Removal of Stopwords: v
 - Removal of Frequent words: Optional
@@ -26,7 +27,14 @@ nltk.download('wordnet')
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 
+def preprocess_data(data):
+    # Choose text >= 20
+    data['Num_words_text'] = data['Text'].apply(lambda x:len(str(x).split())) 
+    df_filtered_reviews = data[(data['Num_words_text'] >=20)]
 
+    # Balacing the review
+    df_sampled = df_filtered_reviews.groupby('Score').apply(lambda x: x.sample(n=10000, random_state = 17)).reset_index(drop = True)
+    return df_sampled
 
 def preprocess_text(text):
     # Lowercasing
@@ -35,6 +43,9 @@ def preprocess_text(text):
     # Removing punctuation
     text = text.translate(str.maketrans('', '', string.punctuation))
     
+    # Removing numbers
+    text = ' '.join(w for w in text.split() if ( not w.isdigit() and  ( not w.isdigit() and len(w)>3)))
+
     # Removing stopwords
     stop_words = set(stopwords.words('english'))
     custom_stopwords = ['i\'m', 'i\'ll']

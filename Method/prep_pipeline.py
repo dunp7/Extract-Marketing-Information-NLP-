492167bb2e745fabc38453db40d1629b2e3b6b26
Dunp <dunp1710@gmail.com>
2024-05-28 16:02:34 +0700

    Update Processing

--- Method/prep_pipeline.py
+++ Method/prep_pipeline.py
@@ -46,37 +46,37 @@ def preprocess_data(data):
     return df_sampled
 
 
+# def preprocess_text(text):
+#     # Lowercasing
+#     text = text.lower()
+    
+#     # Removing punctuation
+#     text = text.translate(str.maketrans('', '', string.punctuation))
+    
+#     # Removing numbers
+#     text = ' '.join(w for w in text.split() if ( not w.isdigit() and  ( not w.isdigit() and len(w)>3)))
+
+#     # Removing stopwords
+#     stop_words = set(stopwords.words('english'))
+#     custom_stopwords = ['i\'m', 'i\'ll']
+#     stop_words.update(custom_stopwords)
+#     text = " ".join([word for word in text.split() if word not in stop_words])
+    
+#     # # Stemming
+#     # stemmer = PorterStemmer()
+#     # text = " ".join([stemmer.stem(word) for word in text.split()])
+    
+#     # Lemmatizing
+#     lemmatizer = WordNetLemmatizer()
+#     text = " ".join([lemmatizer.lemmatize(word) for word in text.split()])
+    
+#     # Tokenizing
+#     tokens = word_tokenize(text)
+    
+#     return tokens
+
+
 def preprocess_text(text):
-    # Lowercasing
-    text = text.lower()
-    
-    # Removing punctuation
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    
-    # Removing numbers
-    text = ' '.join(w for w in text.split() if ( not w.isdigit() and  ( not w.isdigit() and len(w)>3)))
-
-    # Removing stopwords
-    stop_words = set(stopwords.words('english'))
-    custom_stopwords = ['i\'m', 'i\'ll']
-    stop_words.update(custom_stopwords)
-    text = " ".join([word for word in text.split() if word not in stop_words])
-    
-    # # Stemming
-    # stemmer = PorterStemmer()
-    # text = " ".join([stemmer.stem(word) for word in text.split()])
-    
-    # Lemmatizing
-    lemmatizer = WordNetLemmatizer()
-    text = " ".join([lemmatizer.lemmatize(word) for word in text.split()])
-    
-    # Tokenizing
-    tokens = word_tokenize(text)
-    
-    return tokens
-
-
-def preprocess_text_1(text):
     '''Taking only Nouns from reviews'''
     # Lowercasing
     text = text.lower()
